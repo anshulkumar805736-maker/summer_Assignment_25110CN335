600b15c26d3f03e6885757ad9ce7fbfa62124db3
anshulkumar805736-maker <anshul.kumar805736@gmail.com>
2026-06-09 10:16:10 +0530

    assignment

---
+++ Day 12/Q1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int palindrome(int n)
+{
+    int rev = 0, temp = n, rem;
+
+    while(temp != 0)
+    {
+        rem = temp % 10;
+        rev = rev * 10 + rem;
+        temp = temp / 10;
+    }
+
+    return (n == rev);
+}
+
+int main()
+{
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if(palindrome(num))
+        printf("Palindrome Number");
+    else
+        printf("Not a Palindrome Number");
+
+    return 0;
+}
